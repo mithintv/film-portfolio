be6aacb63dd135c50ffc2029917a4001c2464899
Mithin <mithingthomas@gmail.com>
2023-02-02 23:34:57 -0500

    cycle roles

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 
+import { TypeAnimation } from "react-type-animation";
+
 import { css } from "@emotion/react";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
@@ -8,6 +11,7 @@ export default function Header() {
       href="/"
       css={css({
         flexDirection: "column",
+        height: "3rem",
       })}
     >
       <h1
@@ -18,14 +22,43 @@ export default function Header() {
       >
         MITHIN G. THOMAS
       </h1>
-      <h3
+      <TypeAnimation
+        sequence={[
+          "Director", // Types 'One'
+          1000, // Waits 1s
+          "Cinematographer", // Deletes 'One' and types 'Two'
+          1000,
+          "Editor",
+          1000,
+          "Colorist",
+          1000,
+          "2D Animator",
+          1000,
+          "Motion Graphic Designer",
+          1000,
+          "AC",
+          1000,
+          "Grip",
+          1000,
+          () => {
+            // Place optional callbacks anywhere in the array
+          },
+        ]}
+        wrapper="h3"
+        cursor={false}
+        repeat={Infinity}
         css={css({
           fontSize: "0.85rem",
-          margin: "0.15rem 0rem",
+          margin: "0.15rem 0.15rem 0",
         })}
-      >
-        Director & Cinematographer
-      </h3>
+      />
+      {/* <h3
+        css={css({
+          fontSize: "0.85rem",
+          margin: "0.15rem 0.15rem 0",
+        })}
+        className={classes.cycle}
+      ></h3> */}
     </Link>
   );
 }
