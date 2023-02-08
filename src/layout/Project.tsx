9732f8d92e4892d520dff1ef187c418820a81e85
Mithin <mithingthomas@gmail.com>
2023-02-08 16:16:56 -0500

    image to call function after load

--- src/layout/Project.tsx
+++ src/layout/Project.tsx
@@ -12,158 +12,165 @@ import { slideIn } from "../lib/animations";
 
 type AppProps = {
   project: ProjectType;
+  onLoad: () => void;
   feature?: boolean | undefined;
   timeout: number;
   mobile: boolean;
 };
 
 export default function Project(props: AppProps) {
+  const [load, setLoad] = useState(false);
   const containerRef = React.useRef(null);
   const [open, setOpen] = useState<true | false>(false);
   const [details, setDetails] = useState<true | false>(false);
   const roles = props.project.role.split(",");
 
   return (
-    <Fade
-      in
-      timeout={1000}
-      style={{ transitionDelay: `${props.timeout * 100}ms` }}
-    >
-      <Grid
-        item
-        xs={8}
-        sm={4}
-        md={4}
-        css={{
-          padding: "0",
-          width: props.feature ? "inherit" : "33vw",
-          opacity: 1,
-          height: "100%",
-          animation: `${slideIn} 1s ease`,
-          animationDelay: `${props.timeout * 100}ms`,
-        }}
-        onMouseEnter={() => setDetails(true)}
-        onMouseLeave={() => setDetails(false)}
+    <>
+      <Fade
+        in={load}
+        timeout={1000}
+        style={{ transitionDelay: `${props.timeout * 100}ms` }}
       >
-        <AspectRatio
+        <Grid
+          item
+          xs={8}
+          sm={4}
+          md={4}
           css={{
-            width: props.feature ? "80vw" : "100%",
-            maxWidth: "1000px",
+            padding: "0",
+            width: props.feature ? "inherit" : "33vw",
+            opacity: 1,
+            height: "100%",
+            animation: `${slideIn} 1s ease`,
+            animationDelay: `${props.timeout * 100}ms`,
           }}
+          onMouseEnter={() => setDetails(true)}
+          onMouseLeave={() => setDetails(false)}
         >
-          <div
-            ref={containerRef}
+          <AspectRatio
             css={{
-              width: "100%",
-              height: "100%",
-              backgroundColor: "#000",
-              zIndex: 2,
-              cursor: "pointer",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              opacity: details ? 0.75 : 0,
-              transition: "opacity 0.5s ease",
-            }}
-            onClick={() => {
-              setDetails(false);
-              setOpen(true);
+              width: props.feature ? "80vw" : "100%",
+              maxWidth: "1000px",
             }}
           >
-            <Typography
+            <div
+              ref={containerRef}
               css={{
-                color: "#fff",
-                padding: "0 0.5rem",
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#000",
+                zIndex: 2,
+                cursor: "pointer",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                opacity: details ? 0.75 : 0,
+                transition: "opacity 0.5s ease",
               }}
-              variant="h6"
-              align="center"
+              onClick={() => {
+                setDetails(false);
+                setOpen(true);
+              }}
             >
-              {props.project.title}
-            </Typography>
-            {roles.map((role, index) => {
-              return (
-                <Typography
-                  key={index}
-                  css={{
-                    color: "#fff",
-                    display: "block",
-                  }}
-                  variant="caption"
-                  align="center"
-                >
-                  {role}
-                </Typography>
-              );
-            })}
-          </div>
-          <Image
-            css={{
-              objectFit: "cover",
-              transform: details ? "scale(1.1)" : "scale(1.0)",
-              transition: "transform 0.5s ease",
-            }}
-            sizes="(max-width: 768px) 90vw,
+              <Typography
+                css={{
+                  color: "#fff",
+                  padding: "0 0.5rem",
+                }}
+                variant="h6"
+                align="center"
+              >
+                {props.project.title}
+              </Typography>
+              {roles.map((role, index) => {
+                return (
+                  <Typography
+                    key={index}
+                    css={{
+                      color: "#fff",
+                      display: "block",
+                    }}
+                    variant="caption"
+                    align="center"
+                  >
+                    {role}
+                  </Typography>
+                );
+              })}
+            </div>
+            <Image
+              css={{
+                objectFit: "cover",
+                transform: details ? "scale(1.1)" : "scale(1.0)",
+                transition: "transform 0.5s ease",
+              }}
+              sizes="(max-width: 768px) 90vw,
             (max-width: 1200px) 50vw,
             33vw"
-            fill
-            src={props.project.thumbnail}
-            alt={props.project.title}
-          />
-        </AspectRatio>
-        <Modal
-          css={{
-            width: "100vw",
-            backgroundColor: "#000",
-            border: "0px",
-            borderRadius: "0px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-          open={open}
-          onClose={() => setOpen(false)}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box
-            onClick={() => setOpen(false)}
+              fill
+              src={props.project.thumbnail}
+              alt={props.project.title}
+              onLoadingComplete={() => {
+                props.onLoad;
+              }}
+            />
+          </AspectRatio>
+          <Modal
             css={{
+              width: "100vw",
               backgroundColor: "#000",
               border: "0px",
               borderRadius: "0px",
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
-              alignItems: "center",
             }}
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
           >
-            <AspectRatio
-              sx={{
-                width: props.mobile ? "100%" : "75%",
-                maxWidth: "1400px",
+            <Box
+              onClick={() => setOpen(false)}
+              css={{
+                backgroundColor: "#000",
+                border: "0px",
+                borderRadius: "0px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              <ReactPlayer
-                css={{
-                  border: "0px",
-                  borderRadius: "0px",
+              <AspectRatio
+                sx={{
+                  width: props.mobile ? "100%" : "75%",
+                  maxWidth: "1400px",
                 }}
-                width="100%"
-                height="100%"
-                url={props.project.url}
-                playing
-                controls={true}
-              />
-            </AspectRatio>
-            <Box
-              css={{
-                height: "100px",
-                zIndex: -1,
-              }}
-            ></Box>
-          </Box>
-        </Modal>
-      </Grid>
-    </Fade>
+              >
+                <ReactPlayer
+                  css={{
+                    border: "0px",
+                    borderRadius: "0px",
+                  }}
+                  width="100%"
+                  height="100%"
+                  url={props.project.url}
+                  playing
+                  controls={true}
+                />
+              </AspectRatio>
+              <Box
+                css={{
+                  height: "100px",
+                  zIndex: -1,
+                }}
+              ></Box>
+            </Box>
+          </Modal>
+        </Grid>
+      </Fade>
+    </>
   );
 }
